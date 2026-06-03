9db0d076e29cf63d7f2a0b27b98aa4fad7ff2621
yyyyy3 <chloe950302@gmail.com>
2026-06-03 22:38:31 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/night-owl/',   // 設定為您的 GitHub 儲存庫名稱
+  base: '/my-calculator/',
+  plugins: [vue()]
 })
