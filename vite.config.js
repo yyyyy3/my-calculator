0bffe6d7ee58481f9fda49cc20286480a6f348ce
yyyyy3 <chloe950302@gmail.com>
2026-06-04 12:24:17 +0800

    fix: align base path and trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/my-calculator/',
+  base: '/my-calculator/', // 這裡請再次確認是否與 GitHub 儲存庫名稱一致
   plugins: [vue()]
 })
